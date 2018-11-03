287c8361851ba7a9acd859dc09a86d064262dc62
Denis Wong <deniswong@live.ca>
2018-11-03 16:31:42 -0400

    *blows dust off project*
    Added Strength portfolio section

--- resources/views/parts/portfolio/strength.blade.php
+++ resources/views/parts/portfolio/strength.blade.php
@@ -21,7 +21,26 @@
                     <h2 class="text-secondary text-uppercase mb-0">Strength.com</h2>
                     <hr class="star-dark mb-5">
                     <img class="img-fluid mb-5" src="img/portfolio/{{ $portfolioImageName }}.png" alt="">
-                    <p class="mb-5">To be filled in.</p>
+                    <p class="mb-5">The worlds of e-commerce and sports nutition collide. Strength.com sold health supplements for athletes - think protein shakes, pre-workout mixes, fat-cutting pills, and swag for gym junkies and CrossFit faithful. The underpinnings of this site powered Strength.com and related sites, providing for a common foundation to power e-commerce sites.</p>
+                    <h3>Highlights</h3>
+                    <ul class="mb-5">
+                        <li>Did extensive frontend design across Strength and its related sites, by producing views (Laravel blades) and CSS derived from mockups</li>
+                        <li>Wrote API integrations with different payment providers, including Stripe, Newegg, and Amazon Pay</li>
+                        <li>Integrated with third party logistics providers</li>
+                        <li>Did the initial implementation of acceptance testing for our codebase</li>
+                        <li>Numerous bells and whistles in the application's backend: coupon functionality, UI for creating deal pages, delivering digital documents, speed optimization via lazy loading, adding store credits</li>
+                    </ul>
+                    <h3>+EXP Acquired Here</h3>
+                    <ul class="mb-5">
+                        <li>Laravel (and the concepts of MVC frameworks)</li>
+                        <li>The Laravel ecosystem - Forge, Envoyer</li>
+                        <li>Amazon Web Services</li>
+                        <li>Writing web APIs</li>
+                        <li>CSS knowledge, plus SASS3</li>
+                        <li>Managing a LEMP (Linux, Nginx, MySQL, PHP) stack</li>
+                        <li>Less Sublime Text, more PhpStorm please</li>
+                    </ul>
+                    <p class="mb-5"></p>
                     <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
                         <i class="fa fa-close"></i>
                         Close Project</a>
